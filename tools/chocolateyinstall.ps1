6a96013f0d540883b66a822eda872aeeb37a4e1a
Tony Fernandez <tfernandez.sc2@gmail.com>
2022-04-03 11:22:34 -0400

    initial commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageName  = 'insync'
+$url32        = 'https://d2t3ff60b2tol4.cloudfront.net/builds/Insync-3.7.3.50326.exe'
+$checksum32   = '775692791FA971AF5E19C70E56F8FFFB4BE0F2228A568003A94817EB56FD3C27'
+
+$packageArgs = @{
+  packageName   = $packagename
+  fileType      = 'exe'
+  url           = $url32
+  softwareName  = 'Insync'
+  checksum      = $checksum32
+  checksumType  = 'sha256'
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
