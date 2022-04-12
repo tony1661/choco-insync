44a892fbfa65191775db1a8bf2c1025fa6df6b04
Tony Fernandez <tfernandez.sc2@gmail.com>
2022-04-12 08:04:57 -0400

    Updated insync to 3.7.5.50350

--- packages/insync/nupkg/tools/chocolateyinstall.ps1
+++ packages/insync/nupkg/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageName  = 'insync'
-$url32        = 'https://d2t3ff60b2tol4.cloudfront.net/builds/Insync-3.7.4.50336.exe'
-$checksum32   = 'fd21d2b34ec583bf36f96c9e01a1be7be20b21b2d284f0cb97f5e86584c19c77'
+$url32        = 'https://d2t3ff60b2tol4.cloudfront.net/builds/Insync-3.7.5.50350.exe'
+$checksum32   = '14fdf271f83070f3f12d6b65b884260e69940466ef174c9110ceee9f9347d203'
 
 $packageArgs = @{
   packageName   = $packagename
